5f518cf05e4a41d8fff433649226a1275b04075a
uraplayer <uraplayer@mail.ru>
2018-08-26 17:44:58 +0300

    botnamechange

--- src/main/java/com/fatboa/telegram/TelegramBot.java
+++ src/main/java/com/fatboa/telegram/TelegramBot.java
@@ -96,7 +96,7 @@ public class TelegramBot extends TelegramLongPollingBot{
 
     @Override
     public String getBotUsername() {
-        return "githubCVBankbot";
+        return "githubCommitsNotifybot";
     }
 
     @Override
